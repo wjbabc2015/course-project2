f798be2566979318c717f21cd7b44fd7d3f713df
wjbabc2015 <jeremy.wangjb@gmail.com>
2016-02-21 21:07:24 -0500

    update 2.21

--- src/Server.java
+++ src/Server.java
@@ -23,7 +23,7 @@ public class Server {
 	DataInputStream in;
 	DataOutputStream out;
 	
-	List<clientThread> clientList = new ArrayList<clientThread>();
+	List<clientThread> clientList = new ArrayList<clientThread>();//Array List for storing the client sockets
 		
     public static void main(String[] args) {
         // YOUR CODE HERE
@@ -32,19 +32,20 @@ public class Server {
     	new Server().start();
     }
     
+    //Start the server 
     public void start(){
 		try {
-			serverSocket = new ServerSocket(8908);
+			serverSocket = new ServerSocket(8908);//Define server socket with port number
 System.out.println("Server started!");
-			while (true){
+			while (true){ //Monitor client request, start client thread to issue
 				cSocket = serverSocket.accept();
 				
 				clientThread c = new clientThread(cSocket);
 System.out.print("A client connected!\n");
 
-				new Thread(c).start();
+				new Thread(c).start();//Start the client thread
 				
-				clientList.add(c);
+				clientList.add(c);//Add the new client thread in array list
 				}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -57,13 +58,20 @@ System.out.print("A client connected!\n");
 		}
 	}
     
+    /**
+     * Client thread class to solve multiple client
+     * @author Jeremy Wang
+     */
     class clientThread implements Runnable{
     	
     	private Socket s;
     	private DataInputStream myIn = null;
     	private DataOutputStream myOut = null;
     	
-    	
+    	/**
+    	 * Initialize I/O stream
+    	 * @param s socket for client
+    	 */
     	public clientThread(Socket s){
     		this.s = s;
     		try {
@@ -74,6 +82,12 @@ System.out.print("A client connected!\n");
 			}
     	}
     	
+    	/**
+    	 * Send message method 
+    	 * @param msg string of data
+    	 * @throws SocketException
+    	 * Allow server to send message to every client
+    	 */
     	public void sendMsg(String msg)throws SocketException{
     		try {
 				myOut.writeUTF(msg);
@@ -87,13 +101,15 @@ System.out.print("A client connected!\n");
 			clientThread c=null;
 			try{				
 			while (true){
-				String msg = myIn.readUTF();
-System.out.println(msg);		
+				String msg = myIn.readUTF();//Get data stream from client
+System.out.println(msg);
+				//Send the string to all the client within the array list
 				for (int i=0;i<clientList.size(); i++){
 					c = clientList.get(i);
 					c.sendMsg(msg);
 				}
 		}
+	//Exception for taking care the array list, socket, and I/O stream
 	}catch (SocketException e){
 		if (c!=null) clientList.remove(this);
 	}catch (EOFException e){
